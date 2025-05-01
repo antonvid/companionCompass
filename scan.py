b9c015eb73bc74d75cb5266f468b61589c7a2598
antonvid <antonyvidler@gmail.com>
2025-05-01 21:42:12 +0100

    first commit

---
+++ scan.py
@@ -0,0 +1,22 @@
+import asyncio
+from bleak import BleakScanner
+
+async def main():
+    stop_event = asyncio.Event()
+
+    # TODO: add something that calls stop_event.set()
+
+    def callback(device, advertising_data):
+        print(device, advertising_data)
+        pass
+
+    async with BleakScanner(callback) as scanner:
+        ...
+        # Important! Wait for an event to trigger stop, otherwise scanner
+        # will stop immediately.
+        await stop_event.wait()
+
+    # scanner stops when block exits
+    ...
+
+asyncio.run(main())
