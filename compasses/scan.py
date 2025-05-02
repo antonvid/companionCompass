ced80ba099ada7fbd885fbeae575f9b1ae311ef6
antonvid <antonyvidler@gmail.com>
2025-05-02 20:34:21 +0100

    added csv funcionality to scan

--- compasses/scan.py
+++ compasses/scan.py
@@ -1,9 +1,18 @@
 import asyncio
+import csv
 from bleak import BleakScanner
+
+# open CSV for writing RSSI data
+
+csv_file = open("rssi_data.csv", mode="w", newline="")
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(["Beacon", "RSSI"]) # header row
 
 def detection_callback(device, advertisement_data):
     if device.name and device.name.startswith("PicoBeacon"):
         print(f"[FOUND] {device.name or 'Unknown'} - RSSI: {advertisement_data.rssi} - Address: {device.address}")
+        # write data to CSV
+        csv_writer.writerow([device.name, advertisement_data.rssi])
     else:
         pass
 
@@ -16,6 +25,8 @@ async def scan():
             await asyncio.sleep(1)
     except KeyboardInterrupt:
         await scanner.stop()
+    finally:
+        csv_file.close()
 
 if __name__ == "__main__":
     try:
