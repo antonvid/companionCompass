ad27e53c17c257dd2e71f80b32a7cf83333ce748
antonvid <antonyvidler@gmail.com>
2025-05-04 00:51:31 +0100

    change to meters

--- compasses/scan.py
+++ compasses/scan.py
@@ -1,17 +1,21 @@
 import asyncio
 from bleak import BleakScanner
+import time
+
+# Constants for distance calculation
+A = 15.284055464858218
+m = -1.9382076537407522
+
 
 def detection_callback(device, advertisement_data):
+
     if device.name and device.name.startswith("PicoBeacon"):
+
         rssi = abs(advertisement_data.rssi)
-        A = 15.284055464858218
-        m = -1.9382076537407522
-
         dist = 10 ** ((A - rssi) / (10 * m))
+        dist = dist/100
 
-        print(f"[FOUND] {device.name or 'Unknown'} - RSSI: {rssi} - Address: {device.address} - Distance: {dist}cm")
-    else:
-        pass
+        print(f"{dist:.2f}m")
 
 async def scan():
     scanner = BleakScanner(detection_callback)
@@ -19,12 +23,11 @@ async def scan():
     await scanner.start()
     try:
         while True:
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.1)  # Reduced sleep interval for better responsiveness
     except KeyboardInterrupt:
         await scanner.stop()
 
 if __name__ == "__main__":
-
     try:
         asyncio.run(scan())
     except KeyboardInterrupt:
