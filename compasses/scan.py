54f8c18068216cc9cca686c7c4b24d0ddf3033cc
antonvid <antonyvidler@gmail.com>
2025-05-04 00:35:26 +0100

    removed csv functionality

--- compasses/scan.py
+++ compasses/scan.py
@@ -1,18 +1,5 @@
 import asyncio
-import csv
 from bleak import BleakScanner
-
-# Initialize csv_file and csv_writer as None
-csv_file = None
-csv_writer = None
-
-distFile = input("Enter distance from b1: ")
-
-# open CSV for writing RSSI data
-if distFile:
-    csv_file = open(f"rssi_data_{distFile}.csv", mode="w", newline="")
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(["Beacon", "RSSI"]) # header row
 
 def detection_callback(device, advertisement_data):
     if device.name and device.name.startswith("PicoBeacon"):
@@ -23,9 +10,6 @@ def detection_callback(device, advertisement_data):
         dist = 10 ** ((A - rssi) / (10 * m))
 
         print(f"[FOUND] {device.name or 'Unknown'} - RSSI: {rssi} - Address: {device.address} - Distance: {dist}cm")
-        # write data to CSV
-        if csv_writer:
-            csv_writer.writerow([device.name, rssi])
     else:
         pass
 
@@ -38,9 +22,6 @@ async def scan():
             await asyncio.sleep(1)
     except KeyboardInterrupt:
         await scanner.stop()
-    finally:
-        if csv_file:
-            csv_file.close()
 
 if __name__ == "__main__":
 
