97d5ec241ce6854d66b8c172b64bf8b0182b5ce2
antonvid <antonyvidler@gmail.com>
2025-05-15 14:07:14 +0100

    change scan intvl

--- compasses/scan.py
+++ compasses/scan.py
@@ -23,7 +23,7 @@ async def scan():
     await scanner.start()
     try:
         while True:
-            await asyncio.sleep(0.01) 
+            await asyncio.sleep(0.1) 
     except KeyboardInterrupt:
         await scanner.stop()
 
