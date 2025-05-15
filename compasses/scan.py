2ff4ac7335c04a22659e6eb47a3e07d0fd7f1575
antonvid <antonyvidler@gmail.com>
2025-05-15 14:32:04 +0100

    update scan.py

--- compasses/scan.py
+++ compasses/scan.py
@@ -6,29 +6,23 @@ import time
 A = 15.284055464858218
 m = -1.9382076537407522
 
+async def main():
+    stop_event = asyncio.Event()
 
-def detection_callback(device, advertisement_data):
+    def callback(device, advertising_data):
+        if device.name and device.name.startswith("PicoBeacon"):
+            rssi = abs(advertising_data.rssi)
+            dist = 10 ** ((A - rssi) / (10 * m))
+            dist = dist/100
+            
+            print(f"{device.name}: {dist:.2f}m") # print beacon name plus distance estimate
 
-    if device.name and device.name.startswith("PicoBeacon"):
-
-        rssi = abs(advertisement_data.rssi)
-        dist = 10 ** ((A - rssi) / (10 * m))
-        dist = dist/100
-
-        print(f"{device.name}: {dist:.2f}m")
-
-async def scan():
-    scanner = BleakScanner(detection_callback)
-    print("Scanning for BLE beacons... Press Ctrl+C to stop.")
-    await scanner.start()
-    try:
-        while True:
-            await asyncio.sleep(1) 
-    except KeyboardInterrupt:
-        await scanner.stop()
+    async with BleakScanner(callback) as scanner:
+        print("Scanning for BLE beacons... Press Ctrl+C to stop.")
+        try:
+            await stop_event.wait()
+        except KeyboardInterrupt: # stop when Ctrl+C is pressed
+            print("\nStopping scan")
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(scan())
-    except KeyboardInterrupt:
-        print("Stopped by user")
+    asyncio.run(main())
